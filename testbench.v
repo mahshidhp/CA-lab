67bf080bcb6c4402685d98a1cec5daab6cc609e5
mahshidhp <mahshidhpour@gmail.com>
2023-02-03 18:16:35 +0330

    Add images

--- testbench.v
+++ testbench.v
@@ -1,10 +1,10 @@
 `timescale 1ns/1ns
 
 module testbench();
-    reg clk=0, rst=0, forward=1;
+    reg clk=0, rst=0, forward=0;
     arm UUT(clk, rst, forward);
 
-    initial repeat (500) #10 clk=~clk;
+    initial repeat (5000) #10 clk=~clk;
     
     initial begin
       #2 
